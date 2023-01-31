eddabcfae2f489d21155a4f512faadf6e849440a
Maria Saydee Guevarra <saydeeguevarra@gmail.com>
2023-01-31 13:15:47 -0800

    Added run.py

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,18 @@
-from flask import Flask # From the flask module import the Flask class
+# from flask import Flask # From the flask module import the Flask class
 
 
-app= Flask(__name__)    # Create an instance of the Flask class 
-                        # into the app variable (now an object).
+# app= Flask(__name__)    # Create an instance of the Flask class 
+#                         # into the app variable (now an object).
 
 
-@app.get("/")           # Flask decorator that creates routes. 
-def index():            # Flask calls these "view functions".
-    me = {              # Python dictionary with key/value pairs. 
-        "first_name": "Saydee",
-        "last_name": "Guevarra",
-        "hobbies": "cricut",
-        "is_active": True
-    }
+# @app.get("/")           # Flask decorator that creates routes. 
+# def index():            # Flask calls these "view functions".
+#     me = {              # Python dictionary with key/value pairs. 
+#         "first_name": "Saydee",
+#         "last_name": "Guevarra",
+#         "hobbies": "cricut",
+#         "is_active": True
+#     }
 
-    return me           # When a view function returns a dictionary
-                        # Flask automatically converts it to JSON. 
+#     return me           # When a view function returns a dictionary
+#                         # Flask automatically converts it to JSON. 
