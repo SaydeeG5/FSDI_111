322395cd362071ff00865d2e8f6160245309656e
Maria Saydee Guevarra <saydeeguevarra@gmail.com>
2023-01-31 18:29:47 -0800

    lined up notes in init.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,14 +5,14 @@ app= Flask(__name__)    # Create an instance of the Flask class
                         # into the app variable (now an object).
 
 
-@app.route("/aboutme")           # Flask decorator that creates routes. 
+@app.route("/aboutme")  # Flask decorator that creates routes. 
 def index():            # Flask calls these "view functions".
-    me = {              # Python dictionary with key/value pairs. 
+    me = [{             # Python dictionary with key/value pairs. 
         "first_name": "Saydee",
         "last_name": "Guevarra",
         "hobbies": "cricut",
         "is_active": True
-    }
+    }]
 
     return me           # When a view function returns a dictionary
                         # Flask automatically converts it to JSON. 
